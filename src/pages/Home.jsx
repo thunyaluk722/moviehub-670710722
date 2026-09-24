fa56ecb2c3d2eedf7d7e0c40d2e3e1c2e56c5276
Thunyaluk Masatitsup <masatitsup_t@silpakorn.edu>
2026-09-24 05:29:05 +0000

    get movie from api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import FeaturedCarousel from '../components/FeaturedCarousel';
 import { getMovies } from '../api/tmdb';
-//import { movies as localMovies } from '../data/data';
 // TODO ขั้นที่ 5: import { useEffect } from 'react' และ import { getMovies } from '../api/tmdb'
 
 const STEPS = [
